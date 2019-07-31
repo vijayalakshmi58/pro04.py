640eefc8cf94dd25bbce0ee49c307d4460d6c9fa
T.vijayalakshmi <52314223+vijayalakshmi58@users.noreply.github.com>
2019-07-31 10:51:31 +0530

    Update and rename README.md to pro04.py

---
+++ pro04.py
@@ -0,0 +1,12 @@
+# pro04.py
+viji,sen=map(str,input().split())
+wave=0
+if len(bhav)>len(sen):
+  viji,sen=sen,mah
+i=0
+while i<len(viji):
+  wave+=(ord(sen[i])-ord(viji[i]))
+  i+=1
+for i in range(i,len(sen)):
+  wave+=ord(sen[i])-ord('a')+1
+print(wave)
